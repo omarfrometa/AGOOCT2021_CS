2e2ff5efb1a712cf0a01a6f36227152e3ca99d0b
Omar Frometa <omar.frometa@fvtech.net>
2021-09-02 20:06:50 -0400

    Initial Commit

---
+++ CS.WebApplication/Controllers/CareersController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CS.WebApplication;
+
+namespace CS.WebApplication.Controllers
+{
+    public class CareersController : Controller
+    {
+        private ConsSoftEntities db = new ConsSoftEntities();
+
+        // GET: Careers
+        public ActionResult Index()
+        {
+            return View(db.Career.ToList());
+        }
+
+        // GET: Careers/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Career career = db.Career.Find(id);
+            if (career == null)
+            {
+                return HttpNotFound();
+            }
+            return View(career);
+        }
+
+        // GET: Careers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Careers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Seq,Name,Description,Disabled")] Career career)
+        {
+            if (ModelState.IsValid)
+            {
+                career.Id = Guid.NewGuid();
+                career.CreatedDate = DateTime.Now;
+
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = $"{career.Id}_{Path.GetFileName(file.FileName)}";
+                        var filePath = Path.Combine(Server.MapPath("/Public/Careers/"), fileName);
+                        file.SaveAs(filePath);
+
+                        career.LogoUrl = fileName;
+                    }
+                }
+
+                db.Career.Add(career);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(career);
+        }
+
+        // GET: Careers/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Career career = db.Career.Find(id);
+            if (career == null)
+            {
+                return HttpNotFound();
+            }
+            return View(career);
+        }
+
+        // POST: Careers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,Name,Description,Disabled")] Career career)
+        {
+            if (ModelState.IsValid)
+            {
+                career.CreatedDate = DateTime.Now;
+                db.Entry(career).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(career);
+        }
+
+        // GET: Careers/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Career career = db.Career.Find(id);
+            if (career == null)
+            {
+                return HttpNotFound();
+            }
+            return View(career);
+        }
+
+        // POST: Careers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Career career = db.Career.Find(id);
+            db.Career.Remove(career);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CS.WebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CS.WebApplication;
+
+namespace CS.WebApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private ConsSoftEntities db = new ConsSoftEntities();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            var student = db.Student.Include(s => s.Career);
+            return View(student.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            ViewBag.CareerId = new SelectList(db.Career, "Id", "Name");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Seq,Enrollment,FirstName,LastName,Email,Phone,CareerId,Disabled")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Id = Guid.NewGuid();
+                student.CreatedDate = DateTime.Now;
+
+                db.Student.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CareerId = new SelectList(db.Career, "Id", "Name", student.CareerId);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CareerId = new SelectList(db.Career, "Id", "Name", student.CareerId);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,Enrollment,FirstName,LastName,Email,Phone,CareerId,Disabled")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.CreatedDate = DateTime.Now;
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CareerId = new SelectList(db.Career, "Id", "Name", student.CareerId);
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Student student = db.Student.Find(id);
+            db.Student.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
